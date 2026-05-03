029df3acfbdd1ddece99e89efbae26dc1ca62f37
Enovinx <132684434+Enovinx@users.noreply.github.com>
2026-05-03 14:37:31 +1200

    chore: stop tracking build output

--- out/webview/contentSecurityPolicy.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContentSecurityPolicy = createContentSecurityPolicy;
-function createContentSecurityPolicy(nonce) {
-    return `default-src 'none'; style-src 'nonce-${nonce}'; script-src 'nonce-${nonce}';`;
-}
--- out/webview/stateSerializer.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serializeState = serializeState;
-function serializeState(state) {
-    return JSON.stringify(state);
-}
